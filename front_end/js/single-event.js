b5ae44de490354a71ad12aa096f4bf58f23a6ddf
Alex Heinen & Fernanda M M Getschko <pair+aheinen.fmgetschko@devbootcamp.com>
2015-06-22 15:19:05 -0700

    create wishlist associations following a swipe, redirect user to next event page

--- front_end/js/single-event.js
+++ front_end/js/single-event.js
@@ -1,20 +1,34 @@
 $(document).ready(function(event) {
-  console.log('we are here')
+  baseURL = 'http://localhost:3000/'
   $('#container').on('swipeleft', '.image', function(e){
     e.preventDefault();
-    console.log('swiped left!');
+    var data = {accepted: false};
+    createWishlist(data);
   });
 
   $('#container').on('swiperight', '.image', function(e){
     e.preventDefault();
-    console.log('swiped right!');
+    var data = {accepted: true};
+    createWishlist(data);
   });
-
 });
 
-// var createWishlist = function(url, ) {
-//   $.ajax({
-//     url: url,
-
-//   })
-// }
+var createWishlist = function(data) {
+  var user_id = $('#container').attr('class')
+  var event_id = $('.image').attr('id');
+  var url = 'users/' + user_id + '/events/' + event_id + '/wishlists'
+  $.ajax({
+    url: baseURL + url,
+    type: "POST",
+    data: data
+  })
+  .done(function(response){
+    template = Handlebars.compile($("#single-event-template").html());
+    var desc = "<p>Description: " + response.event.description + "</p>";
+    $("#container").html(template(response));
+    $('#container a').append(desc);
+  })
+  .fail(function(){
+    alert('fail');
+  })
+}
--- whereto_server/app/views/users/edit.json.jbuilder
+++ whereto_server/app/views/users/edit.json.jbuilder
@@ -2,6 +2,7 @@ json.categories @categories do |c|
   json.name c.name
   json.description c.description
   json.selected c.selected(@interests)
+  json.image c.image
 end
 
 json.user @user
